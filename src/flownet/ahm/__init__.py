59d2c49358ca845f5ab23806b42b1fc04c11f0c5
Ubuntu <egona@VM-Eduardo.430u41ciwbmupm0cbb0cjpnibd.fx.internal.cloudapp.net>
2020-05-07 15:10:48 +0000

    Moves accuracy metric functions to new module file

--- src/flownet/ahm/__init__.py
+++ src/flownet/ahm/__init__.py
@@ -2,6 +2,8 @@ from ._assisted_history_matching import (
     AssistedHistoryMatching,
     delete_simulation_output,
     save_iteration_parameters,
+)
+from ._ahm_iteration_analytics import (
     save_iteration_analytics,
 )
 from ._run_ahm import run_flownet_history_matching
