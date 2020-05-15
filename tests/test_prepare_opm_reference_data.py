35115ad45634e311f1413aba74a580ab130c7c4c
Ubuntu <egona@VM-Eduardo.430u41ciwbmupm0cbb0cjpnibd.fx.internal.cloudapp.net>
2020-05-15 12:35:01 +0000

    Fixes assertion of objects

--- tests/test_prepare_opm_reference_data.py
+++ tests/test_prepare_opm_reference_data.py
@@ -4,8 +4,11 @@ import pandas as pd
 from flownet.ahm._ahm_iteration_analytics import prepare_opm_reference_data
 
 
-def test_prepare_flownet_data() -> None:
+def test_prepare_opm_reference_data() -> None:
     data = {"key_1": [1, 2], "key_2": [3, 4]}
-    assert prepare_opm_reference_data(
-        pd.DataFrame(data, columns=["key_1", "key_2"]), "key_", 2
-    ) == np.array([[1, 1], [2, 2], [3, 3], [4, 4]])
+    assert np.allclose(
+        prepare_opm_reference_data(
+            pd.DataFrame(data, columns=["key_1", "key_2"]), "key_", 2
+        ),
+        np.array([[1, 1], [2, 2], [3, 3], [4, 4]]),
+    )
