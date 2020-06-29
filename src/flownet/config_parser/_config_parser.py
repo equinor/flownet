d53cca17a4070ed7f2619e24d1cd0920b52d28c6
Ubuntu <opl@ddpd-opl.430u41ciwbmupm0cbb0cjpnibd.fx.internal.cloudapp.net>
2020-06-29 12:28:40 +0000

    removed unnecessary .with_suffix('')

--- src/flownet/config_parser/_config_parser.py
+++ src/flownet/config_parser/_config_parser.py
@@ -548,7 +548,6 @@ def parse_config(configuration_file: pathlib.Path) -> ConfigSuite.snapshot:
     for suffix in [".DATA", ".EGRID", ".UNRST", ".UNSMRY", ".SMSPEC"]:
         if (
             not pathlib.Path(config.flownet.data_source.eclipse_case)
-            .with_suffix("")
             .with_suffix(suffix)
             .is_file()
         ):
