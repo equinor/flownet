8ba681037ce85ac98bb09999ca7c4b740e24994f
flownet-github-action <flownet-github-action>
2020-09-08 14:52:06 +0000

    Update Github Pages

---
+++ _static/configsuite_ext.js
@@ -0,0 +1,26 @@
+/* Copyright 2018 Equinor ASA and The Netherlands Organisation for
+Applied Scientific Research TNO.
+
+Licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the conditions stated in the LICENSE file in the project root for
+details.
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+*/
+
+ $(function() {
+    $( ".cs_header" ).click(function(event) {
+        var target = $( event.target );
+        var children = target.parent().parent().find(">.cs_content");
+
+        children.toggle();
+    });
+});
