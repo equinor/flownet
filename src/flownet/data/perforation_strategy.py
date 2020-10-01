429d0bdc6be810c6135d7bd3a489ccaeaf59e604
Wouter J. de Bruin <wouterjdb@hotmail.com>
2020-10-01 16:03:42 +0200

    Fixed black issue

--- src/flownet/data/perforation_strategy.py
+++ src/flownet/data/perforation_strategy.py
@@ -1,3 +1,6 @@
+from typing import List
+
+import numpy as np
 import pandas as pd
 
 
@@ -17,10 +20,9 @@ def bottom_point(df: pd.DataFrame) -> pd.DataFrame:
     df_multiple_ever_true = (
         df_multiple.groupby(["X", "Y", "Z", "WELL_NAME"]).sum().reset_index()
     )
-    df_multiple_true = df_multiple.loc[df_multiple["OPEN"]]
     df_first_dates = (
-        df_multiple_true[["WELL_NAME", "DATE"]]
-        .sort_values("DATE")
+        df[["WELL_NAME", "DATE"]]
+        .sort_values(["WELL_NAME", "DATE"])
         .groupby(["WELL_NAME"])
         .first()
     )
@@ -50,10 +52,9 @@ def top_point(df: pd.DataFrame) -> pd.DataFrame:
     df_multiple_ever_true = (
         df_multiple.groupby(["X", "Y", "Z", "WELL_NAME"]).sum().reset_index()
     )
-    df_multiple_true = df_multiple.loc[df_multiple["OPEN"]]
     df_first_dates = (
-        df_multiple_true[["WELL_NAME", "DATE"]]
-        .sort_values("DATE")
+        df[["WELL_NAME", "DATE"]]
+        .sort_values(["WELL_NAME", "DATE"])
         .groupby(["WELL_NAME"])
         .first()
     )
@@ -96,21 +97,6 @@ def multiple(df: pd.DataFrame) -> pd.DataFrame:
         )
         | (df["WELL_NAME"] != df["WELL_NAME"].shift(1))
     ]
-
-
-def time_avg_open_location_multiple_based_on_workovers(
-    df: pd.DataFrame,
-) -> pd.DataFrame:
-    """
-
-
-    Args:
-        df:
-
-    Returns:
-
-    """
-    return time_avg_open_location(multiple_based_on_workovers(df))
 
 
 # pylint: disable=too-many-locals
@@ -198,14 +184,23 @@ def multiple_based_on_workovers(df: pd.DataFrame) -> pd.DataFrame:
                     (df_w_groups["X"] < xmin_foreign)
                     | (df_w_groups["Y"] < ymin_foreign)
                     | (df_w_groups["Z"] < zmin_foreign)
-                ), 'GROUPID'] = (df_w_groups["GROUPID"].max() + 1)
+                ),
+                ["GROUPID"],
+            ] = (
+                df_w_groups["GROUPID"].max() + 1
+            )
+
             df_w_groups.loc[
                 (df_w_groups["GROUPID"] == groupid)
                 & (
                     (df_w_groups["X"] > xmax_foreign)
-                    | (df_w_groups["Y"] < ymax_foreign)
-                    | (df_w_groups["Z"] < zmax_foreign)
-                ), 'GROUPID'] = (df_w_groups["GROUPID"].max() + 1)
+                    | (df_w_groups["Y"] > ymax_foreign)
+                    | (df_w_groups["Z"] > zmax_foreign)
+                ),
+                ["GROUPID"],
+            ] = (
+                df_w_groups["GROUPID"].max() + 1
+            )
 
     df_w_groups["OPEN"] = df_w_groups["OPEN"].astype(int)
     result = df_w_groups.groupby(["WELL_NAME", "DATE", "GROUPID"]).mean().reset_index()
@@ -215,21 +210,6 @@ def multiple_based_on_workovers(df: pd.DataFrame) -> pd.DataFrame:
     return result
 
 
-def multiple_based_on_workovers_time_avg_open_location(
-    df: pd.DataFrame,
-) -> pd.DataFrame:
-    """
-
-
-    Args:
-        df:
-
-    Returns:
-
-    """
-    return time_avg_open_location(multiple_based_on_workovers(df))
-
-
 def time_avg_open_location(df: pd.DataFrame) -> pd.DataFrame:
     """
 
@@ -237,78 +217,62 @@ def time_avg_open_location(df: pd.DataFrame) -> pd.DataFrame:
         df:
 
     Returns:
-
-    def multi_xyz_append(append_obj_list):
-        for global_conn in append_obj_list[1]:
-            coords.append(
-                [append_obj_list[0], *self._grid.get_xyz(ijk=global_conn.ijk())]
-            )
+    """
 
     coords: List = []
-    for well_name in self._wells.allWellNames():
-        global_conns = self._wells[well_name][0].globalConnections()
+    for well_name in df["WELL_NAME"].unique():
         coord_append = coords.append
-        if self._perforation_handling_strategy == "bottom_point":
-            xyz = self._grid.get_xyz(ijk=global_conns[-1].ijk())
-        elif self._perforation_handling_strategy == "top_point":
-            xyz = self._grid.get_xyz(ijk=global_conns[0].ijk())
-        elif self._perforation_handling_strategy == "multiple":
-            xyz = [global_conns]
-            coord_append = multi_xyz_append
-
-        elif self._perforation_handling_strategy == "multiple_based_on_workovers":
-
-            pass
-
-        elif self._perforation_handling_strategy == "time_avg_open_location":
-            connection_open_time = {}
-
-            for i, conn_status in enumerate(self._wells[well_name]):
-                time = datetime.datetime.strptime(
-                    str(conn_status.simulationTime()), "%Y-%m-%d %H:%M:%S"
-                )
-                if i == 0:
-                    prev_time = time
-
-                for connection in conn_status.globalConnections():
-                    if connection.ijk() not in connection_open_time:
-                        connection_open_time[connection.ijk()] = 0.0
-                    elif connection.isOpen():
-                        connection_open_time[connection.ijk()] += (
-                            time - prev_time
-                        ).total_seconds()
-                    else:
-                        connection_open_time[connection.ijk()] += 0.0
-
+        connection_open_time = {}
+        for index, row in (
+            df[df["WELL_NAME"] == well_name]
+            .sort_values("DATE", ascending=True)
+            .iterrows()
+        ):
+            time = row["DATE"]
+            if index == 0:
                 prev_time = time
+            xyz = (row["X"], row["Y"], row["Z"])
 
-            xyz_values = np.zeros((1, 3), dtype=np.float64)
-            total_open_time = sum(connection_open_time.values())
-
-            if total_open_time > 0:
-                for connection, open_time in connection_open_time.items():
-                    xyz_values += np.multiply(
-                        np.array(self._grid.get_xyz(ijk=connection)),
-                        open_time / total_open_time,
-                    )
+            if xyz not in connection_open_time:
+                connection_open_time[xyz] = 0.0
+            elif row["OPEN"]:
+                connection_open_time[xyz] += (time - prev_time).total_seconds()
             else:
-                for connection, open_time in connection_open_time.items():
-                    xyz_values += np.divide(
-                        np.array(self._grid.get_xyz(ijk=connection)),
-                        len(connection_open_time.items()),
-                    )
+                connection_open_time[xyz] += 0.0
 
-            xyz = tuple(*xyz_values)
+            prev_time = time
 
+        xyz_values = np.zeros((1, 3), dtype=np.float64)
+        total_open_time = sum(connection_open_time.values())
+
+        if total_open_time > 0:
+            for connection, open_time in connection_open_time.items():
+                xyz_values += np.multiply(
+                    np.array(connection),
+                    open_time / total_open_time,
+                )
         else:
-            raise Exception(
-                f"perforation strategy {self._perforation_handling_strategy} unknown"
-            )
+            for connection, open_time in connection_open_time.items():
+                xyz_values += np.divide(
+                    np.array(connection),
+                    len(connection_open_time.items()),
+                )
+
+        xyz = tuple(*xyz_values)
 
         coord_append([well_name, *xyz])
 
-    return pd.DataFrame(
-        coords, columns=["WELL_NAME", "X", "Y", "Z", "START_DATE", "END_DATE"]
+    result = pd.DataFrame(coords, columns=["WELL_NAME", "X", "Y", "Z"])
+    result["OPEN"] = True
+    result["DATE"] = None
+
+    df_first_dates = (
+        df[["WELL_NAME", "DATE"]]
+        .sort_values(["WELL_NAME", "DATE"])
+        .groupby(["WELL_NAME"])
+        .first()
     )
-    """
-    return df
+
+    return result[["WELL_NAME", "X", "Y", "Z", "OPEN"]].merge(
+        df_first_dates, on="WELL_NAME"
+    )
