f4e6ef3d7e9865d8f6dea5d99303f9177f51e897
Eduardo Barros <eduardo.barros@tno.nl>
2020-11-11 18:05:11 +0000

    Fixes analysis workflow for flat BHP in Egg model

--- src/flownet/ahm/_ahm_iteration_analytics.py
+++ src/flownet/ahm/_ahm_iteration_analytics.py
@@ -116,10 +116,15 @@ def normalize_data(data_opm_reference, data_ensembles_flownet):
             tmp = np.append(tmp, data_ens, axis=1)
 
     matrix_data = np.append(data_opm_reference, tmp, axis=1)
-    scale = 1 / (
-        np.tile(data_opm_reference.max(), matrix_data.shape[0])
-        - np.tile(data_opm_reference.min(), matrix_data.shape[0])
-    )
+
+    if data_opm_reference.max() == data_opm_reference.min():
+        scale = 1 / (np.tile(data_opm_reference.max(), matrix_data.shape[0]))
+    else:
+        scale = 1 / (
+            np.tile(data_opm_reference.max(), matrix_data.shape[0])
+            - np.tile(data_opm_reference.min(), matrix_data.shape[0])
+        )
+
     norm_matrix_data = (
         matrix_data * scale[:, None]
         - (np.tile(data_opm_reference.min(), matrix_data.shape[0]) * scale)[:, None]
