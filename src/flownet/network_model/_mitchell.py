0b19d5d2d7d7a999ca56b83b25b9a546178eaff4
Lonneke van Bijsterveldt <lonneke.vanbijsterveldt@tno.nl>
2020-11-16 16:11:23 +0000

    Revert logging to print to check if tests pass

--- src/flownet/network_model/_mitchell.py
+++ src/flownet/network_model/_mitchell.py
@@ -48,13 +48,7 @@ def mitchell_best_candidate_modified_3d(
     """
     np.random.seed(random_seed)
     start = time.time()
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    console.setFormatter(logging.Formatter("%(message)s"))
-    logger.addHandler(console)
-    logging.info("Adding flow nodes:  0%")
+    print("Adding flow nodes:  0%", end=" ")
 
     # Read list of coordinate tuples and convert to 1D-numpy arrays
     x, y, z = (np.asarray(t) for t in zip(*perforations))
@@ -107,8 +101,9 @@ def mitchell_best_candidate_modified_3d(
         mid = time.time()
         if mid - start > 4:
             start = mid
-            logging.info(
-                f"\rAdding flow nodes:  {int(((i-num_points)/num_added_flow_nodes)*100)}%"
+            print(
+                f"\rAdding flow nodes:  {int(((i-num_points)/num_added_flow_nodes)*100)}%",
+                end="",
             )
 
         in_hull = np.asarray([False] * num_candidates)
@@ -201,7 +196,7 @@ def mitchell_best_candidate_modified_3d(
         y = np.append(y, y_candidate[best_candidate])
         z = np.append(z, z_candidate[best_candidate])
 
-    logging.info("\rAdding flow nodes:  100% \ndone.")
+    print("\rAdding flow nodes:  100%\ndone.")
 
     # Return the real/original and added flow node coordinates as a list of tuples.
     return [(x[i], y[i], z[i]) for i in range(len(x))]
