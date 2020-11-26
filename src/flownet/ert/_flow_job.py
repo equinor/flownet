31c512adb4c87df909cca71cf9a518a24bb0c410
Manuel Borregales <manuelantoniobr@gmail.com>
2020-11-26 23:14:46 +0100

    Checking is /usr/bin/flow is in enviroment and cheking if flow is in path

--- src/flownet/ert/_flow_job.py
+++ src/flownet/ert/_flow_job.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import subprocess
 from pathlib import Path
+import shutil
 
 from ert_shared.plugins.plugin_response import plugin_response
 from ert_shared.plugins.plugin_manager import hook_implementation
@@ -26,8 +27,24 @@ def run_flow():
     parser.add_argument("data_file", type=str)
     args = parser.parse_args()
 
-    subprocess.run(
-        [os.environ.get("FLOW_PATH", "/usr/bin/flow"), args.data_file], check=True
-    )
+    if "FLOW_PATH" in os.environ:
+        flow_path = os.environ.get("FLOW_PATH")
+        if os.path.isfile(flow_path):
+            # Runing flow from ENV variable
+            subprocess.run([flow_path, args.data_file], check=True)
+        else:
+            print("Error: FLOW_PATH point to a path that doesn't exist")
+    elif shutil.which("flow") is None:
+        print("Error: flow is not installed")
+    else:
+        flow_path = shutil.which("flow")
+        if os.path.isfile(flow_path):
+            # Runing flow from Installation variable
+            subprocess.run(
+                [os.environ.get("FLOW_PATH", shutil.which("flow")), args.data_file],
+                check=True,
+            )
+        else:
+            print("Error: Instalation FLOW_PATH points to a path that doesn't exist")
 
     Path("FLOW_SIMULATION.OK").write_text("")
