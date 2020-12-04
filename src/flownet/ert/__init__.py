d81a3833a742a95bff96c7c487a66cd739b20b97
Eduardo Gonçalves Dias de Barros <36161163+edubarrosTNO@users.noreply.github.com>
2020-12-04 11:54:15 +0000

    Adds resampling of observations (#272)
    * Adds resampling of observations
    
    * Adds if-statement for resampling entry in create_obs_file
    
    * Updates test_check_obsfiles_ert_yaml
    
    * Fixes pylint for test_check_obsfiles_ert_yaml
    
    * Fixes test_check_obsfiles_ert_yaml for pytest
    
    * Updates CHANGELOG.md
    
    * Addresses review comments
    
    * Addresses review comments (2)
    
    Co-authored-by: Wouter J. de Bruin <9119793+wouterjdb@users.noreply.github.com>

--- src/flownet/ert/__init__.py
+++ src/flownet/ert/__init__.py
@@ -2,3 +2,4 @@ from ._create_synthetic_refcase import create_synthetic_refcase
 from ._create_ert_setup import create_ert_setup
 from ._run_subprocess import run_ert_subprocess
 from ._create_ert_setup import create_observation_file
+from ._create_ert_setup import resample_schedule_dates
