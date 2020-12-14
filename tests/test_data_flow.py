52f16e21c3f8e03f5ddea2df27ab8630e42cefe3
Wouter J. de Bruin <wouterjdb@hotmail.com>
2020-12-14 12:22:18 +0100

    Made test model agnostic

--- tests/test_data_flow.py
+++ tests/test_data_flow.py
@@ -1,11 +1,15 @@
+import glob
+import operator
 from pathlib import Path
 
 from numpy.testing import assert_almost_equal
+from ecl.grid import EclRegion
 
 from flownet.data.from_flow import FlowData
 
-DATA_FILE = Path.home() / Path("flownet-testdata/norne/input_model/norne/NORNE_ATW2013")
-
+DATA_FILE = Path(
+    glob.glob(str(Path(Path.home() / "flownet-testdata/*/input_model/*/*.DATA")))[0]
+)
 
 # pylint: disable=protected-access
 def test_grid_cell_bounding_boxes() -> None:
@@ -14,19 +18,32 @@ def test_grid_cell_bounding_boxes() -> None:
         "multiple_based_on_workovers",
     )
 
-    flowdata._layers = [(1, 22)]
+    # Test no argument and entire field being equal
+    flowdata._layers = [(1, flowdata.grid.nz)]
     assert_almost_equal(
         flowdata._grid_cell_bounding_boxes(), flowdata._grid_cell_bounding_boxes(0)
     )
 
+    # Test zero'th layer id
     flowdata._layers = [(1, 2), (3, 4)]
     result = flowdata._grid_cell_bounding_boxes(0)
-    assert result.shape[0] == 4455
+    active_cells = EclRegion(flowdata.grid, True)
+    active_cells.select_kslice(
+        *tuple(map(operator.sub, flowdata._layers[0], (1, 1))), intersect=True
+    )
+    active_cells.select_active(intersect=True)
+    assert result.shape[0] == active_cells.active_size()
     assert result.shape[1] == 6
 
-    flowdata._layers = [(1, 2), (3, 22)]
+    # Test last layer id
+    flowdata._layers = [(1, 2), (3, 4)]
     result = flowdata._grid_cell_bounding_boxes(1)
-    assert result.shape[0] == 39976
+    active_cells = EclRegion(flowdata.grid, True)
+    active_cells.select_kslice(
+        *tuple(map(operator.sub, flowdata._layers[-1], (1, 1))), intersect=True
+    )
+    active_cells.select_active(intersect=True)
+    assert result.shape[0] == active_cells.active_size()
     assert result.shape[1] == 6
 
     assert True
