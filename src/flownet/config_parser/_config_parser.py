527e0d0225679990a52dafd9dc44c41f8d5f03a0
Wouter J. de Bruin <wouterjdb@hotmail.com>
2021-01-14 12:21:12 +0100

    Add angle to configparser

--- src/flownet/config_parser/_config_parser.py
+++ src/flownet/config_parser/_config_parser.py
@@ -424,6 +424,12 @@ def create_schema(config_folder: Optional[pathlib.Path] = None) -> Dict:
                         MK.Type: types.String,
                         MK.Default: "RATE",
                     },
+                    "angle_threshold": {
+                        MK.Type: types.Number,
+                        MK.Default: 150,
+                        MK.Description: "Angle threshold used, after Delaunay triagulation,"
+                        "to remove sides/tubes opposite angles larger than the supplied treshold.",
+                    },
                     "hyperopt": {
                         MK.Type: types.NamedDict,
                         MK.Content: {
