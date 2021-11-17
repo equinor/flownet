904d04b320c7c5a8d8f7a5cf2877bca3603ed6ef
August Johansson <august.johansson@sintef.no>
2021-11-18 00:57:04 +0100

    Change tests a bit

--- tests/test_coarse_grid.py
+++ tests/test_coarse_grid.py
@@ -1,10 +1,12 @@
-from typing import List, Dict
+from typing import List, Tuple
 import numpy as np
-from flownet.coarse_model import CoarseGrid, create_egrid
+import pytest
+
+from flownet.coarse_model import CoarseGrid, create_egrid, Tree
 from ecl.grid import EclGrid
 
 
-def get_coarse_grid(partition: List) -> Dict:
+def get_coarse_grid(partition: List) -> Tuple:
     data_dir = "./tests/data/"
 
     grid_file = data_dir + "faarikaal2.EGRID"
@@ -18,40 +20,72 @@ def get_coarse_grid(partition: List) -> Dict:
         well_coords = np.reshape(well_coords, (1, well_coords.shape[0]))
 
     CG = CoarseGrid(ecl_grid, well_coords, partition)
-    cg_dict = CG.create_coarse_grid()
-    cg = create_egrid(
-        cg_dict["coordinates"],
-        cg_dict["num_nodes"],
-        cg_dict["num_elements"],
-        cg_dict["actnum"],
-    )
-    return cg, well_coords, cg_dict, CG
+    cg = CG.create_coarse_grid()
+    return cg, well_coords, CG
 
 
-def test_egrid_construction() -> None:
-    cg, _, _, _ = get_coarse_grid([1, 1, 1])
-    num_elements = cg.nx * cg.ny * cg.nz
-    num_nodes = (cg.nx + 1) * (cg.ny + 1) * (cg.nz + 1)
-    assert cg["num_elements"] == num_elements
-    assert cg["num_nodes"] == num_nodes
-
-
-def test_coarse_grid_bbox() -> None:
-    cg, _, _, CG = get_coarse_grid([1, 1, 1])
-    bboxmin, bboxmax = CG.create_bbox()
-    x = cg["coordinates"]
+def check_bbox(coordinates, bboxmin, bboxmax) -> None:
     tol = 1e-3
     for d in range(3):
-        assert abs(x[d][0] - bboxmin[d]) < tol
-        assert abs(x[d][-1] - bboxmax[d]) < tol
+        assert abs(coordinates[d][0] - bboxmin[d]) < tol
+        assert abs(coordinates[d][-1] - bboxmax[d]) < tol
 
 
-def test_coarse_grid_points() -> None:
-    cg, well_coords, _, _ = get_coarse_grid([1, 1, 1])
+def check_tree(num_elements, tree) -> None:
+    assert np.prod(num_elements) == len(tree.get_leaves())
+
+
+def test_coarse_grid() -> None:
+    # Check that we have reasonable values at the various stages
+    cg, _, CG = get_coarse_grid([1, 1, 1])
+
+    # 1.
+    bboxmin, bboxmax = CG.create_bbox()
+    tree = Tree(bboxmin, bboxmax)
+
+    # 2.
+    CG.split(tree)
+
+    # 3.
+    CG.refine(tree)
+
+    # 4.
+    tree = CG.smooth(tree)
+
+    # 5.
+    actnum = CG.remove_cells_outside(tree)
+
+    # 6.
+    coordinates, num_nodes, num_elements = CG.extract_grid_data(tree)
+
+    # Checks
+    check_bbox(coordinates, bboxmin, bboxmax)
+    check_tree(num_elements, tree)
+
+
+def check_one_well_per_cell(cg, well_coords) -> bool:
     num_elements = cg.nx * cg.ny * cg.nz
     cnt = np.zeros(num_elements, dtype=int)
     for well_coord in well_coords:
-        ijk = cg.find_cell(wc[0], wc[1], wc[2])
+        ijk = cg.find_cell(well_coord[0], well_coord[1], well_coord[2])
         assert ijk is not None
         cnt[cg.get_global_index(ijk)] += 1
-    assert max(cnt) == 1
+    return max(cnt) == 1
+
+
+def test_one_well_per_cell() -> None:
+    # Test that there are only one well per cell in the coarse grid
+    cg, well_coords, _ = get_coarse_grid([1, 1, 1])
+    assert check_one_well_per_cell(cg, well_coords)
+
+
+@pytest.mark.xfail
+def test_well_setup() -> None:
+    # Test of a particular well setup that can be problematic
+    dims = (1, 1, 1)
+    dV = (1, 1, 1)
+    ecl_grid = EclGrid.create_rectangular(dims, dV)
+    well_coords = np.array([[0.25, 0.25, 0.5], [0.25, 0.75, 0.5], [0.75, 0.5, 0.5]])
+    CG = CoarseGrid(ecl_grid, well_coords, [1, 1, 1])
+    cg = CG.create_coarse_grid()
+    assert check_one_well_per_cell(cg, well_coords)
